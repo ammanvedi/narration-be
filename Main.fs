6f3335d649d9c4b3adddc0d9a3417254e98626d5
Amman Vedi <av@Ammans-MacBook-Pro.local>
2023-04-10 17:20:54 +0100

    chore: initial

---
+++ Main.fs
@@ -0,0 +1,26 @@
+﻿open FSharpPlus
+open System.Net
+
+let uploadFile(path: string)
+
+type Err = string
+
+let succ: Result<string, Err> = Ok "abc"
+
+let pureFunc(s: string) = s 
+
+let fail(arg: string): Result<string, Err> = Error "sds"
+
+let maybeWithSideFx = monad' { 
+    let! p = succ
+    let! q = fail p
+    let! r  =  applicative.Return (pureFunc q)
+    return r
+}
+
+[<EntryPoint>]
+let main args =
+    match maybeWithSideFx with
+        | Ok x -> printfn "OK : %s" x
+        | Error s -> printfn "Error : %s" s
+    0
